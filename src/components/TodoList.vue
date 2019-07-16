11e36ea497bc08ae0e557a1c3e8d22548982a817
amanat-juwel <amanatjuwel@gmail.com>
2019-07-16 11:46:53 +0600

    initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      {{todo.title}}
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+      todos: [
+        {
+          'id': 1,
+          'title': 'Finish Vue Screencast',
+          'completed': false,
+          'editing': false,
+        },
+        {
+          'id': 2,
+          'title': 'Take over world',
+          'completed': false,
+          'editing': false,
+        },
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+        editing: false,
+      })
+      this.newTodo = ''
+      this.idForTodo++
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+</style>
